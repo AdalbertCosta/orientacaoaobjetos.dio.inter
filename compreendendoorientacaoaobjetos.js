3eecd92d2b7029891e379730e2134108f85bd4fb
AdalbertCosta <contato@adalbertocosta.com>
2022-01-12 06:55:08 -0300

    conclusão

--- compreendendoorientacaoaobjetos.js
+++ compreendendoorientacaoaobjetos.js
@@ -1,1 +1,27 @@
 //Paradigmas e Pilares
+
+//Paradigmas
+
+/*
+Os programas são "objetos" que possuem uma série de proprieades.
+Pilares: Herança - Poliformismo - Encapsulamento - Abstração.
+
+*/
+
+//Pilares
+
+/* Abstração
+"Processo menal que consiste em isolar um aspecto determinado de um estado de coisas relativamente complexo, a fim de simplificar a sua avaliação, classificação ou para permitir a comunicação do mesmo."
+*/
+
+/*Herança
+O objeto filho herda propriedades e métodos do objeto pai.
+*/
+
+/* Encapsulamento
+Cada classe tem propriedades e métodos independentes do restante do código.
+*/
+
+/* Poliformismo
+Objetos podem herdar a mesma classe pai, mas se comportarem de forma diferente quando invocamos seus metódos.
+*/
