1847c7313751844c35b3b5b3ad718c325b678190
AdalbertCosta <contato@adalbertocosta.com>
2022-01-14 06:27:32 -0300

    Constructor

---
+++ atividade.js
@@ -0,0 +1,18 @@
+// Atividade - Conta Bnacária
+
+class ContaBnContaBancaria {
+  constructor(agencia, numero, tipo, saldo) {
+    this.agencia = agencia;
+    this.numero = numero;
+    this.tipo = tipo;
+    this._saldo = saldo;
+  }
+
+  get saldo() {
+    return this._saldo;
+  }
+
+  set saldo(valor) {
+    this._saldo = valor;
+  }
+}
